fb7bbf2cc1785ddb9fc444725370eceafbea954c
juanpesttom <user@INFORBACH15.localdomain>
2020-11-04 09:46:59 +0100

    ejs

--- ejs/25.c
+++ ejs/25.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+
+bool suma_4(int M[4][4])
+{
+    int i, j, sum;
+    
+    sum = 0;
+    for (i = 0; i < 4; i++, sum = 0)
+    {
+        for (j = 0; j < 4; j++)
+            sum += M[i][j];
+        if (sum == 4)
+            return true;
+    }
+    
+    sum = 0;
+    for (i = 0; i < 4; i++, sum = 0)
+    {
+        for (j = 0; j < 4; j++)
+            sum += M[j][i];
+        if (sum == 4)
+            return true;
+    }
+    return false;
+}
+
+void rellenar_ceros(int M[4][4])
+{
+    int i, j;
+    
+    srand(time(NULL));
+    do {
+        for (i = 0; i < 4; i++)
+            for (j = 0; j < 4; j++)
+                M[i][j] = rand() % 2;
+    } while (suma_4(M));
+}
+
 
 int main(int argc, char *argv[])
 {
+    int M[4][4], i, j;
+
+    rellenar_ceros(M);
+    for (i = 0; i < 4; i++)
+    {
+        for (j = 0; j < 4; j++)
+            printf("%d ", M[i][j]);
+        putchar('\n');
+    }
     return 0;
 }
--- ejs/26.c
+++ ejs/26.c
@@ -1,5 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
+void swap(char *p1, char *p2)
+{
+    char p[strlen(p1+1)];
+
+    strcpy(p, p1);
+    strcpy(p1, p2);
+    strcpy(p2, p);
+    
+    
+}
+void bubblesort(char **p)
+{
+    int i, j;
+
+    for (i = 1; i < 10; i++)
+        for (j = 0; j < 10-i-1; j++)
+            if (strlen(p[j]) > strlen(p[j+1]))
+                swap(p[j], p[j+1]);
+}
 int main(int argc, char *argv[])
 {
     return 0;
