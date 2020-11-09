91afcd4aa0ef99a62518f7a0fec11a4d13b03241
juanpesttom <user@INFORBACH15.localdomain>
2020-11-09 13:31:48 +0100

    ejs

--- ejs/29.c
+++ ejs/29.c
@@ -1,6 +1,54 @@
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
+
+int num_vocales(char *p)
+{
+    int n, i;
+    char c;
+    
+    n = i = 0;
+    while (p[i] != '\0')
+    {   
+        c = tolower(p[i++]);
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            n++;
+    }
+    return n;
+}
+
+int sortfunct(const void *v1,const void *v2)
+{
+    char **p1 = (char **) v1;
+    char **p2 = (char **) v2;
+    
+    return num_vocales(*p1) - num_vocales(*p2);
+}
+
+void ordenar_vocales(char *p[10])
+{
+    qsort(p, 10, sizeof(char *), sortfunct);
+}
 
 int main(int argc, char *argv[])
 {
+    char *p[10] = {
+        "aaaaaaaaaa",
+        "aaaaaaaaa",
+        "aaaaaaaa",
+        "aaaaaaaaaa",
+        "aaaasdfsasdf",
+        "aasdfgs",
+        "afgbdfgh",
+        "efgfg",
+        "ifgjf",
+        "a",
+    };
+    int i;
+    ordenar_vocales(p);
+
+    for (i = 0; i < 10; i++)
+        printf("%s\n", p[i]);
     return 0;
 }
--- ejs/30.c
+++ ejs/30.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define LEN 5
+
+// Devuelve un puntero al mayor número de un vector
+int *mayor_int(int *p, int len)
+{
+    int h, i, *n;
+    i = 0;
+    h = p[i];
+
+    for (i += 1; i < len; i++)
+        if (p[i] > h)
+            h = p[i];
+    n = malloc(sizeof(int));
+    *n = h;
+    return n;
+}
 
 int main(int argc, char *argv[])
 {
+    int *p;
+    int l[LEN] = {1,2,3,4,5};
+
+    p = mayor_int(l, LEN);
+    printf("%d\n", *p);
+    free(p);
     return 0;
 }
